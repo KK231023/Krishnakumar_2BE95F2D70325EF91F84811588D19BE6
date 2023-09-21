a548d95effab2eac6eec7f9cc431bfebe6d0b5f7
KK231023 <144245856+KK231023@users.noreply.github.com>
2023-09-21 13:17:47 +0530

    Add files via upload

---
+++ Unit 3 Challenges/main (6).py
@@ -0,0 +1,32 @@
+class Student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+    Student("Harish", "A456", 7.9),
+    Student("Jagan", "A457", 8.6),
+    Student("Surya", "A458", 9.5),
+    Student("Krishna kumar", "A459", 9.8),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
